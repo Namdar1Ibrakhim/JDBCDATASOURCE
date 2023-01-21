c73acaf2b6c2246ff1b12057276b9e8f0802e78b
Ibrakhim <113717353+Namdar1Ibrakhim@users.noreply.github.com>
2023-01-21 16:30:36 +0500

    Add files via upload

---
+++ src/main/java/com/example/jdbsdatasourse/JdbsDataSourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbsdatasourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbsDataSourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbsDataSourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jdbsdatasourse/JdbsDataSourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbsdatasourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbsDataSourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
